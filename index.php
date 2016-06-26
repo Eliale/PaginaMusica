1ce0ac28709af6d151a9f48b6cbcf3789e7abc4f
Eliale <elialejandros@gmail.com>
2016-06-25 21:22:16 -0500

    MP3 dESCARGA

--- index.php
+++ index.php
@@ -1,86 +1,110 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <script type="text/javascript" src="js/main.js"></script>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>        
-        <!-- Loading the Deezer SDK -->
-        <script type="text/javascript" src="http://cdn-files.deezer.com/js/min/dz.js"></script>
-        <link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen" />
+<head>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>        
+    <!-- Loading the Deezer SDK -->
+    <script type="text/javascript" src="http://cdn-files.deezer.com/js/min/dz.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen" />
 
-        <title>Tu Musica</title>
-    </head>
-    <body  onload= "topViral('Zoé');lastfm('Zoé');search('Zoé');youtube('Zoé');top_ten('Zoé');">
+    <title>Tu Musica</title>
+</head>
+<body  onload= "topViral('Zoé');lastfm('Zoé');search('Zoé');top_ten('Zoé');youtube('Zoé');get_mp3('Zoé');">
+
+    <form id="the-form">
+
+        <input  type="text" id="texto" placeholder="busca tu artista">
+        <input type="button" id="buscar" value="buscar" onclick= "main()">
+
+    </form>     
+
+    <div>
+        <div id="topten">
+            <h2>Top 10</h2>
+        </div>
+        <div id="top">
+            <h2>Artistas relacionados</h2>
+        </div> 
+    </div> 
+
+
+    <div id="busqueda">   
+        <div class="col-sm-6" id="last"></div>
+        <div id="deezer" ></div>
+
+
+    </div>
+    <div id="reproductor">
+        <div id="dz-root"></div>
+        <h2>Reproductor de musica</h2>
+        <div id="player" align="center"></div>
+    </div>
+
+    <div id="youtube">
+
+    </div>  
+
+    <div id ="descargar">
+        <h2>DESCARGAR</h2>
 
         <form>
 
-            <input  type="text" id="texto" placeholder="busca tu artista">
-            <input type="button" id="buscar" value="buscar" onclick= "main()">
+            <input  type="text" id="cancion" placeholder="busca tu cancion">
+            <input type="button" id="buscar_cancion" value="buscar_cancion" onclick= "main_2()">
             
-        </form>     
+        </form>  
+        <div id="bajar">
 
-         <div>
-            <div id="topten">
-                <h2>Top 10</h2>
-            </div>
-            <div id="top">
-                <h2>Artistas relacionados</h2>
-            </div> 
-         </div> 
 
-        <div id="busqueda">   
-            <div class="col-sm-6" id="last"></div>
-            <div id="deezer" ></div>
-            
-            
         </div>
-        <div id="reproductor">
-            <div id="dz-root"></div>
-            <h2>Reproductor de musica</h2>
-            <div id="player" align="center"></div>
-            </div>
+    </div> 
 
-         <div id="youtube">
-         
-         </div>   
-    </body>
-    <script>
-        $(document).ready(function () {
-            $("#controlers input").attr('disabled', true);
-            $("#slider_seek").click(function (evt, arg) {
-                var left = evt.offsetX;
-                DZ.player.seek((evt.offsetX / $(this).width()) * 100);
-            });
-        });
-        function event_listener_append() {
-            var pre = document.getElementById('event_listener');
-            var line = [];
-            for (var i = 0; i < arguments.length; i++) {
-                line.push(arguments[i]);
-            }
-            pre.innerHTML += line.join(' ') + "\n";
-        }
-        function onPlayerLoaded() {
-            $("#controlers input").attr('disabled', false);
-            event_listener_append('player_loaded');
-            DZ.Event.subscribe('current_track', function (arg) {
-                event_listener_append('current_track', arg.index, arg.track.title, arg.track.album.title);
-            });
-            DZ.Event.subscribe('player_position', function (arg) {
-                event_listener_append('position', arg[0], arg[1]);
-                $("#slider_seek").find('.bar').css('width', (100 * arg[0] / arg[1]) + '%');
-            });
-        }
-        DZ.init({
-            appId: '8',
-            channelUrl: 'http://developers.deezer.com/examples/channel.php',
-            player: {
-                container: 'player',
-                cover: true,
-                playlist: true,
-                width: 650,
-                height: 300,
-                onload: onPlayerLoaded
-            }
-        });
-    </script>
+
+    <div id="map">
+
+    </div>
+</body>
+
+<script>
+$(document).ready(function () {
+    $("#controlers input").attr('disabled', true);
+    $("#slider_seek").click(function (evt, arg) {
+        var left = evt.offsetX;
+        DZ.player.seek((evt.offsetX / $(this).width()) * 100);
+    });
+});
+function event_listener_append() {
+    var pre = document.getElementById('event_listener');
+    var line = [];
+    for (var i = 0; i < arguments.length; i++) {
+        line.push(arguments[i]);
+    }
+    pre.innerHTML += line.join(' ') + "\n";
+}
+function onPlayerLoaded() {
+    $("#controlers input").attr('disabled', false);
+    event_listener_append('player_loaded');
+    DZ.Event.subscribe('current_track', function (arg) {
+        event_listener_append('current_track', arg.index, arg.track.title, arg.track.album.title);
+    });
+    DZ.Event.subscribe('player_position', function (arg) {
+        event_listener_append('position', arg[0], arg[1]);
+        $("#slider_seek").find('.bar').css('width', (100 * arg[0] / arg[1]) + '%');
+    });
+}
+DZ.init({
+    appId: '8',
+    channelUrl: 'http://developers.deezer.com/examples/channel.php',
+    player: {
+        container: 'player',
+        cover: true,
+        playlist: true,
+        width: 650,
+        height: 300,
+        onload: onPlayerLoaded
+    }
+});
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDH5603UZ1k1ARBwKxyVhlqhuhDB16UJwM"></script>
 </html>
--- js/main.js
+++ js/main.js
@@ -11,9 +11,22 @@
         topViral(valor);
         top_ten(valor);
         youtube(valor);
+        get_mp3(valor);
     }
 }
 ;
+
+ function main_2() {
+    var valor = document.getElementById("cancion").value;
+    if (valor === "") {
+        alert("no se puede hacer una busqueda vacia xD");
+    } else {
+       get_mp3(valor);
+    }
+}
+;
+
+
 function lastfm(valor) {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&lang=es&artist=" + valor + "&api_key=ae9dc375e16f12528b329b25a3cca3ee&format=json", true);
@@ -23,13 +36,11 @@ function lastfm(valor) {
             var bio = JSON.parse(xhttp.responseText);
             console.log(bio.artist.bio.summary.length);
             if (bio.artist.bio.summary.length < 90) {
-                console.log("no hay resultados");
                 $("#last").append("<h2>no hay resultados en last fm para esa busqueda</h2>");
             } else {
                 var imagenes = bio.artist.image;
                 var imagen;
                 for (var i = 0; i < imagenes.length; i++) {
-                    console.log(imagenes[i].size);
                     if (imagenes[i].size === "mega") {
                         imagen = imagenes[i]["#text"];
                     }
@@ -87,6 +98,7 @@ function search(term) {
 
     });
 }
+
 function topViral(valor) {
     $("#top").empty();
     document.getElementById("top").innerHTML = "<h2>Artistas Relacionados</h2>";
@@ -124,6 +136,43 @@ function busquedaTable(boton) {
 }
 ;
 
+function get_mp3(valor){
+      $("#bajar").empty();
+        console.log("Cancion buscada"+valor);
+        var xhttp = new XMLHttpRequest();
+        var url = 'https://www.googleapis.com/youtube/v3/search?q='+valor+'&part=snippet&maxResults=25&search_sort=video_avg_rating&key=AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE';
+        xhttp.open("GET", url, true);
+        xhttp.setRequestHeader ("Accept", "application/json");
+        xhttp.send();
+        var r=0;
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+             var datos = JSON.parse(xhttp.responseText);
+             for (var i = 0; i < 25; i++) {
+               if(datos.items[i].id.videoId !== undefined && _es_vevo(datos.items[i].snippet.channelTitle) === true){
+                    var id_video=datos.items[i].id.videoId;
+                   // var etiqueta ="<a href='//www.youtubeinmp3.com/fetch/?video=https://www.youtube.com/watch?v="+id_video+"' style='text-decoration:none;color:#03a730;'><img src='//www.youtubeinmp3.com/icon/download.png' style='vertical-align:middle;'> <strong>Download MP3</strong></a>";
+                   var codigo="<iframe style='width:230px;height:60px;border:0;overflow:hidden;' scrolling='no' src='//www.youtubeinmp3.com/widget/button/?video=https://www.youtube.com/watch?v="+id_video+"'&color=c91818>";
+                   
+                 //  $("#descargar").append(etiqueta);
+                  // $("#descargar").append("<br>");
+                   $("#bajar").append(codigo);
+                   $("#bajar").append("<br>");
+                //  break;
+                r++;
+              }
+          }
+          if(r===0){
+             $("#bajar").append("<label> NO HAY RESULTADOS</label>");
+          }               
+      } 
+      console.log("fierro");
+}
+};
+
+
+
+
 function top_ten(valor){
     $("#topten").empty();
     var xhttpt = new XMLHttpRequest();
@@ -134,17 +183,14 @@ function top_ten(valor){
         if (xhttpt.readyState === 4 && xhttpt.status === 200) {
             var resp = JSON.parse(xhttpt.responseText);  
             var mejores_canciones = resp.toptracks.track;
-
             for (var i = 0; i < mejores_canciones.length; i++) {
                 var nombre = mejores_canciones[i].name;
                 var pagina = mejores_canciones[i].url;
-                var num_rep=mejores_canciones[i].playcount;
-               // $("#topten").append("<input type='image' name='" + nombre + "' SRC='" + mejores_canciones[i].image[4]["#text"] + "' ALT='" + mejores_canciones[i].name + "' WIDTH=40 HEIGHT=40 onclick= 'busquedaTable(this)'>");
+                var num_rep = mejores_canciones[i].playcount;
                 $("#topten").append("<label>" + mejores_canciones[i].name + "</label><label>Reproducciones: "+num_rep+"</label>");
                 $("#topten").append("<a href='" + pagina + ">ir a last</a>");
                 $("#topten").append("<br>");
             }
-
     }
 };
 };
@@ -179,7 +225,6 @@ function youtube(valor){
                     frames.setAttribute('style', 'width: 350px; height: 250px; margin-top: 50px');
                     you_elem.appendChild(frames);
                     v[i] = document.getElementById('v2' + (i + 1));
-                    console.log(id_video[i]);
                     v[i].setAttribute('src', 'https://www.youtube.com/embed/' + id_video[i]);
                 }
             }            
@@ -190,4 +235,57 @@ function youtube(valor){
 
 var _es_vevo = function(titulo){
    return (/VEVO/.test(titulo));
+};
+
+
+
+
+
+function initMap(){      
+        var link= $('#the-form').serialize();
+        var nueva = link.replace('q=','');
+        var nueva2 = nueva.replace('+','%20');
+
+        $.ajax({
+            url: "http://api.bandsintown.com/artists/"+nueva2+"/events.json?api_version=2.0&app_id=MUSIC&date=upcoming",
+            data: null,
+            type: "GET",
+            crossDomain: true,
+            dataType: 'jsonp',
+
+            success: function(result){
+                $("#map").append("<h1>No hay eventos proximos</h1>");
+                $("#map").append("<p>");
+
+                var myLatLng = {lat: result[0].venue.latitude, lng: result[0].venue.longitude};
+                var map = new google.maps.Map(document.getElementById('map'), { zoom: 5,
+                    center: myLatLng,
+                    RotateControlOptions:true,
+                    mapTypeControl:true,
+                    mapTypeControlOptions: {
+                        style:google.maps.MapTypeControlStyle.DROPDOWN_MENU
+
+
+
+                    }
+                });
+
+                var tamano=result.length;
+
+                for( i = 0; i < tamano; i++ ) {
+                    var position = new google.maps.LatLng(result[i].venue.latitude,result[i].venue.longitude);
+                    marker = new google.maps.Marker({
+                        position: position,
+                        map: map,
+                        icon:'http://img.getjar.mobi/icon-50x50/ba/852391_thm.png',
+                        animation:google.maps.Animation.BOUNCE,
+                        title: result[i].title+" "+result[i].formatted_datetime
+                    });
+
+                    var infowindow = new google.maps.InfoWindow({
+                        content: result[i].title+" "+result[i].formatted_datetime
+                    });
+                }
+            }
+        })
 };
